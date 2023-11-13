57f2a64b27e83af053d115ef7be881e83583b8b9
neeeal <matira.nealbartonjames@gmail.com>
2023-11-13 22:54:24 +0800

    added profile page and profile component for reusability

--- SkaninMobileApp/components/Profile.js
+++ SkaninMobileApp/components/Profile.js
@@ -1,116 +1,67 @@
-import { StyleSheet, View, Image, Text, TouchableOpacity, Button } from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; 
+import { StyleSheet, View, Image, Text, TouchableOpacity, Button } from 'react-native'; 
+import ProfileContainer from './components/ProfileContainer';
+import BigHeadline from './components/BigHeadline';
 
 export default function Profile({navigation}) {
     return(
         <View style={styles.pageContainer}>
-            <View style={styles.headerContainer}>
-                <AntDesign name="left" size={24} color="black" />
-                <Image style={{ width: 24, height: 24 }} 
-                source={require('../assets/baseline-search-24px.svg')} />
-            </View>
-            <View stype={styles.topContainer}>
-                <Text style={styles.headerText}>My Profile </Text>
-            </View>
-
-            <View stype={styles.profileContainer}>
-                <Text>Matilda Brown</Text>
-                <Text>matildabrown@mail.com</Text>
+            <BigHeadline Headline={"My Profile"}></BigHeadline>
+            <View style={styles.profileContainer}>
+                <Image 
+                source={require('../assets/sampleUser.png')}  
+                style={{width: '69px', height: '69px', borderRadius: 69/ 2}} 
+                />
+                <Text style={styles.nameText}>Matilda Brown</Text>
+                <Text style={styles.emailText}>matildabrown@mail.com</Text>
             </View>
 
-            <View stype={styles.itemContainer}>
-                <Text>My Scans</Text>
-                <Text>Already have 12 scans</Text>
-            </View>
-
-            <View stype={styles.itemContainer}>
-                <Text>Address</Text>
-                <Text>Mindoro</Text>
-            </View>
-
-            <View stype={styles.itemContainer}>
-                <Text>Contact Number</Text>
-                <Text>09876543211</Text>
-            </View>
-
-            <View stype={styles.itemContainer}>
-                <Text>Reviews</Text>
-                <Text>Reviews for 4 items</Text>
-                <AntDesign name="right" size={24} color="black" />
-            </View>
-
-            <View stype={styles.itemContainer}>
-                <Text>Settings</Text>
-                <Text>Notifications, password</Text>
-            </View>
-
-                <Button 
+            <ProfileContainer itemText={"My scans"} subitemText={"Already have 12 scans"}></ProfileContainer>
+            <ProfileContainer itemText={"Full Name"} subitemText={"Alicia Batumbakal"}></ProfileContainer>
+            <ProfileContainer itemText={"Address"} subitemText={"Mindoro"}></ProfileContainer>
+            <ProfileContainer itemText={"Contact Number"} subitemText={"09876543211"}></ProfileContainer>
+            <ProfileContainer itemText={"My reviews"} subitemText={"Reviews for 4 items"}></ProfileContainer>
+            <ProfileContainer itemText={"Settings"} subitemText={"Notifications, password"}></ProfileContainer>
+            
+            <Button 
                     title = 'Home'
                     onPress={() => navigation.navigate('Homepage')}
-                />
+             />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     pageContainer: {
-        flex:1,
+        display: 'flex',
         backgroundColor: '#FFFFFF',
     },
-    headerContainer: {
-        display: 'flex',
-        width: '375px',
-        height: '44px',
-        padding: '8px 343px 12px 8px',
-        alignItems: 'center',
-        flexShrink: 0,
-    },
-    topContainer: {
-        flex: 1,
-        alignItems: 'center',
-    },
     profileContainer: {
-        flex: 1
+        flex: 3,
+        width: '100%',
+        height: '812px',
+        borderRadius: '20px',
+        background: '#F9F9F9',
     },
     itemContainer: {
         flex: 1,
-        alignItems: 'flex-end',
-        justifyContent: 'space-evenly'
+        flexDirection:'row',
+        // alignItems: 'flex-end',
+        justifyContent: 'space-between'
     },
-    headerText:{
-        flex:1,
-        color: '#222222',
+    nameText:{
+        color: '#222',
         fontFamily: 'Montserrat',
-        fontSize: '34px',
+        fontSize: '18px',
+        fontStyle: 'italic',
+        fontWeight: '100',
+        lineHeight: '22px', /* 122.222% */
+    },
+    emailText:{
+        color: '#9B9B9B',
+        fontFamily: 'Montserrat',
+        fontSize: '14px',
         fontStyle: 'normal',
-        fontWeight: '700',
-        lineHeight: 'normal'
-    },
-    itemText: {
-        fontSize: 16,
-        color:'#9B9B9B',
-    },
-    subitemText: {
-        fontSize: 11,
-        color:'#9B9B9B',
+        fontWeight: '500',
+        lineHeight: '20px', /* 142.857% */
     }
-    // appTitle: {
-    //   fontSize: '42px',
-    //   marginTop: '-4vh',
-    //   fontFamily: 'Ultra-Regular',
-    //   color: '#D49A42'
-    // },
-    // continueContainer: {
-    //   flex:1,
-    // },  
-    // continueText: {
-    //     fontFamily: 'mosterratt-medium',
-    //     fontSize: '13px',
-    //     color: '#222222',
-    // },
-    // touchable: {
-    //     margin:0,
-    //     flex:1,
-    //     paddding:0
-    //   },
 }) 
